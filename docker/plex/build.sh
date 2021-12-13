556863c1e7b25444b54552a6b36f5dfebe9a482d
Felix Meschberger <felix@meschberger.ch>
2021-12-13 22:12:01 +0100

    Make build script more generic

---
+++ docker/plex/build.sh
@@ -0,0 +1,29 @@
+#!/bin/env sh
+
+GIT_DIR="checkout"
+GIT_REPO="plexinc/pms-docker"
+GIT_BRANCH="master"
+
+DOCKERFILE="Dockerfile.armv7"
+
+REGISTRY="registry.meschberger.ch"
+REPOSITORY="${REGISTRY}/${GIT_REPO}"
+
+LATEST="${REPOSITORY}:latest"
+VERSION="${REPOSITORY}:$(date +%Y%m%d)"
+
+# checkout or update the plexinc/pms-docker
+if [ -d ${GIT_DIR} ] ; then
+  cd ${GIT_DIR}
+  git checkout "${GIT_BRANCH}"
+  git pull --rebase
+else
+  git clone --branch "${GIT_BRANCH}" https://github.com/${GIT_REPO} ${GIT_DIR}
+  cd ${GIT_DIR}
+fi
+
+# build the container
+docker build -t ${LATEST} -t ${VERSION} -f "${DOCKERFILE}" .
+
+# push the container to my registry
+docker image push --all-tags "${REPOSITORY}"
