85545c52897809ffee8047596755f09d10b6470b
Felix Meschberger <felix@meschberger.ch>
2023-10-14 09:58:10 +0200

    Add new unifi-network-application with separate mongo service

---
+++ compose/unifi-network-application/init-mongo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This file is source-d after MongoDB has started the first time
+# that is we have access to all environment variables and functions
+# of the startup script...
+#
+# Context used in this script is
+#  $f        the path to this script
+#  file_env  a function to resolve a VAR_FILE to VAR
+
+__ENV_FILE__="$(dirname ${f})/.env"
+if [ -r "${__ENV_FILE__}" ] ; then
+  echo "Reading environment from ${__ENV_FILE__}"
+  . "${__ENV_FILE__}"
+else
+  echo "Environment file ${__ENV_FILE__} does not exist"
+  echo "Continuing with additional environment, script may fail"
+fi
+
+# Support reading Unifi user name and password from a file
+file_env 'UNIFI_MONGO_USER'
+file_env 'UNIFI_MONGO_PASS'
+
+for suffix in "" "_stat" ; do 
+  __DB__="${UNIFI_MONGO_DBNAME}${suffix}"
+  echo "Creating user ${UNIFI_MONGO_USER} in DB ${__DB__}"
+  __SCRIPT__="db.createUser({user: \"${UNIFI_MONGO_USER}\", pwd: \"${UNIFI_MONGO_PASS}\", roles: [{role: \"readWrite\", db: \"${__DB__}\"}]});"
+  "${mongo[@]}" "${__DB__}" --eval "${__SCRIPT__}"
+done
